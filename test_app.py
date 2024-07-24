08546afb7c30ba8c249d067034a62cdda0db2ba8
snirkap <snirwork1@gmail.com>
2024-07-24 13:59:52 +0300

    Add initial tests and update CI workflow

---
+++ test_app.py
@@ -0,0 +1,12 @@
+from app import app
+import pytest
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_hello(client):
+    rv = client.get('/')
+    assert rv.data == b'Hello, World!'
